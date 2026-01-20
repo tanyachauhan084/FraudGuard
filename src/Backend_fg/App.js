38ce6f1368628736ae1fae4b595a4bbddc74a60d
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-01-20 16:14:19 +0530

    Changes Added

--- src/Backend_fg/App.js
+++ src/Backend_fg/App.js
@@ -1,6 +1,11 @@
-import express from "express"
+import express from "express";
+import healthcheckroute from "./routes/healthcheck.routes.js";
 
 const app= express();
 
 
+
+app.use("/via/api/healthcheck/yes", healthcheckroute);
+
 export default app;
+
--- src/Backend_fg/server.js
+++ src/Backend_fg/server.js
@@ -1,19 +1,22 @@
-
+import app from "./App.js";
 import dotenv from "dotenv";
-import app from "../Backend_fg/App.js";
-
+import db_Connection from "./db/db_connection.js";
 
 dotenv.config();
 
 
-const port= process.env.PORT|| 8000;
+const port= process.env.PORT ||8000;
 
-app.listen(port, ()=>{
-    console.log("It is Connected");
+
+db_Connection()
+.then(()=>{
+    app.listen(port, ()=>{
+        console.log("Server is listening now okayyy");
+    })
+    })
+.catch((err)=>{
+console.log(err);
+
 })
-
-
-
-
 
 
