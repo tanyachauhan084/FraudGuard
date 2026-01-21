23c9add9a2ba7f7c8c57e36b189fd30660e08a72
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-01-21 08:49:56 +0530

    added in-built, application level middlewares

--- src/Backend_fg/App.js
+++ src/Backend_fg/App.js
@@ -1,9 +1,24 @@
 import express from "express";
-import healthcheckroute from "./routes/healthcheck.routes.js";
+import cors from "cors";
+
 
 const app= express();
 
 
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({limit:"16kb"}));
+app.use(express.static("public"));
+
+app.use(cors({
+    origin: process.env.ORIGIN,
+    credentials:true,
+    methods:["GET", "POST", "UPDATE", "PUT", "PATCH"],
+    allowedHeaders:["Authorization", "Content-type"],
+}))
+
+
+
+import healthcheckroute from "./routes/healthcheck.routes.js";
 
 app.use("/via/api/healthcheck/yes", healthcheckroute);
 
